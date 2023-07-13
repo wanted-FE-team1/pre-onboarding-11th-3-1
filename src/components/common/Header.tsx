a23a48b0ffd8bfc97c869901008fbbbe84f2f873
Sumin Sohn <soyw31@gmail.com>
2023-07-13 15:49:54 +0900

    [design] 로딩 스피너 css 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className='header p-6 bg-blue-500 text-white'>
+    <header className='header p-6 pl-16 bg-blue-500 text-white text-xl mb-5'>
       <h1>Facebook / React</h1>
     </header>
   );
--- src/pages/IssueList.tsx
+++ src/pages/IssueList.tsx
@@ -1,9 +1,10 @@
 import { useContext, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IssueContext } from '../contexts/IssueContext';
+import LoadingSpinner from '../components/common/LoadingSpinner';
 
 export default function IssueList() {
-  const { issues, getIssues, loading } = useContext(IssueContext);
+  const { issues, getIssues, isLoading } = useContext(IssueContext);
   const obsRef = useRef(null);
   const [page, setPage] = useState(0);
 
@@ -29,43 +30,43 @@ export default function IssueList() {
     getIssues(page);
   }, [page]);
 
-  console.log('issues :>> ', issues);
-
   return (
     <div className='max-w-xl m-auto'>
-      <p>이슈리스트</p>
-      <ul className='divide-y divide-solid'>
-        {issues?.map((issue: any, index: any) => (
-          <div key={issue.id}>
-            <li>
-              <Link
-                to={`/issue/${issue.id}`}
-                className='flex justify-between items-center hover:bg-slate-300  p-3'
-              >
-                <div>
-                  <h2 className='text-base'>{`#${issue.number} ${issue.title}`}</h2>
-                  <p className='text-sm'>
-                    {`작성자: ${issue.user?.login}, `}
-                    {`작성일: ${issue.created_at}`}
-                  </p>
-                </div>
-                <p>{`코멘트: ${issue.comments}`}</p>
-                {/* 프로필이미지: issue.user.avatar_url
-                    본문: issue.body */}
-              </Link>
-            </li>
-            {index % 5 === 4 && (
-              <Link to='https://www.wanted.co.kr/'>
-                <img
-                  className='m-auto p-5'
-                  src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100'
-                  alt='Ad'
-                />
-              </Link>
-            )}
-          </div>
-        ))}
-        {loading && <div className='spinner'>로딩 중..</div>}
+      <ul>
+        {issues?.map(
+          (
+            issue: any,
+            index: number, // TODO createContext 타입 지정해야됨. 그럼 any 빼도 됨..
+          ) => (
+            <div key={issue.id}>
+              <li className='border-solid border-b-2 border-gray-200 '>
+                <Link
+                  to={`/issue/${issue.id}`}
+                  className='flex justify-between items-center hover:bg-slate-300  p-3'
+                >
+                  <div>
+                    <h2 className='text-base'>{`#${issue.number} ${issue.title}`}</h2>
+                    <p className='text-sm'>
+                      {`작성자: ${issue.user?.login}, `}
+                      {`작성일: ${issue.created_at}`}
+                    </p>
+                  </div>
+                  <p>{`코멘트: ${issue.comments}`}</p>
+                </Link>
+              </li>
+              {index % 5 === 4 && (
+                <Link to='https://www.wanted.co.kr/'>
+                  <img
+                    className='m-auto p-5'
+                    src='https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100'
+                    alt='Ad'
+                  />
+                </Link>
+              )}
+            </div>
+          ),
+        )}
+        {isLoading && <LoadingSpinner />}
         <li ref={obsRef}></li>
       </ul>
     </div>
