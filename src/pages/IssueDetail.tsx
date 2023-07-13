b4b4239cc76e4cd5e678317bc7508d58a05a4b84
Sumin Sohn <soyw31@gmail.com>
2023-07-13 13:19:14 +0900

    [feat] 이슈 상세 페이지 작성

--- src/pages/IssueDetail.tsx
+++ src/pages/IssueDetail.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { IssueContext } from '../contexts/IC';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import remarkGfm from 'remark-gfm';
 
 export default function IssueDetail() {
+  const { getIssue } = useContext(IssueContext);
   const { id } = useParams();
 
-  // Fetch the issue based on the id, or use a context
-  //    const issue = ...;
+  const issue = getIssue(Number(id));
 
   return (
-    <div>
-      {/* <h2>{issue.title}</h2>
-      <p>{issue.author}</p>
-      <p>{issue.date}</p>
-      <p>{issue.comments}</p>
-      <ReactMarkdown>{issue.body}</ReactMarkdown>
-      <img src={issue.avatarUrl} alt={`${issue.author}'s avatar`} /> */}
+    <div className='max-w-xl m-auto'>
+      <h2 className='flex justify-between items-center hover:bg-slate-300  p-3 border-b-2'>
+        <div className='w-14 h-14 rounded-lg overflow-hidden mr-4'>
+          <img
+            className='w-full h-full object-cover'
+            src={issue.user.avatar_url}
+            alt='깃헙 프로필 이미지'
+          />
+        </div>
+        <div>
+          <h2 className='text-base'>{`#${issue.number} ${issue.title}`}</h2>
+          <p className='text-sm'>
+            {`작성자: ${issue.user?.login}, `}
+            {`작성일: ${issue.created_at}`}
+          </p>
+        </div>
+        <p>{`코멘트: ${issue.comments}`}</p>
+        {/* 프로필이미지: issue.user.avatar_url
+                    본문: issue.body */}
+      </h2>
+      <div className='p-3'>
+        <ReactMarkdown remarkPlugins={[remarkGfm]}>{issue.body}</ReactMarkdown>
+      </div>
     </div>
   );
 }
