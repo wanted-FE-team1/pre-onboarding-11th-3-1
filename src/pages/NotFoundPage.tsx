0a34ead9d45ebf15021315dca86b4036487c7198
hihijin <117073214+hihijin@users.noreply.github.com>
2023-07-13 17:04:04 +0900

    feat: 에러페이지 구현

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div className='flex h-screen w-full flex-col items-center justify-center text-gray-800'>
+      <div className='mb-10 text-4xl font-bold'>404 Not Found</div>
+    </div>
+  );
+}
+
+export default ErrorPage;
