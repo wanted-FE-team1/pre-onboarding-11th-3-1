819286e50e9c99e9d4b84d1777369406a69b3a86
박희진 <117073214+hihijin@users.noreply.github.com>
2023-07-13 23:22:01 +0900

    feat: useLocation 추가 import

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,8 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 export default function Header() {
   const locationNow = useLocation();
-	if (locationNow.pathname === '/error') return null;
+  if (locationNow.pathname === '/error') return null;
   let {owner, repo} = useParams();
   return (
     <header className='header p-6 pl-16 bg-blue-500 text-white text-xl mb-5'>
