9e0fd3aeed413a81266917790274bce129885900
박희진 <117073214+hihijin@users.noreply.github.com>
2023-07-13 23:23:09 +0900

    design: header 레이아웃 정중앙으로 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
   if (locationNow.pathname === '/error') return null;
   let {owner, repo} = useParams();
   return (
-    <header className='header p-6 pl-16 bg-blue-500 text-white text-xl mb-5'>
+    <header className='header p-6 pl-16 bg-blue-500 text-white text-xl mb-5 flex items-center justify-center'>
       <h1>{owner}/{repo}</h1>
     </header>
   );
