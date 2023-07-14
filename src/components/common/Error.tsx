c37dd56d67d20cf78385e7b72d15493096ae8bd9
who0803 <10rlatkdtn@naver.com>
2023-07-14 11:46:27 +0900

    Feat: Implement Error component
    에러 컴포넌트 구현

---
+++ src/components/common/Error.tsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className='bg-gray-200 p-6 rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 shadow-lg'>
+      <h1 className='text-2xl font-bold mb-2'>에러가 발생했습니다.</h1>
+    </div>
+  );
+}
